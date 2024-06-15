ddc0acd35a69c2dcfd5af975d3483f0c48d494c8
LeticiaBapFelix <leticiafelix2799@gmail.com>
2024-06-15 18:47:31 -0300

    Utilizando ambientes virtuais e API

---
+++ app.py
@@ -0,0 +1,11 @@
+import requests
+
+url = 'https://guilhermeonrails.github.io/api-restaurantes/restaurantes.json'
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    dados_json = response.json()
+    print(dados_json)
+else:
+    print(response.status_code)
